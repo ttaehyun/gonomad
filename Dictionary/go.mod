b0d1d29f8e831ae84efdb51673b5b05d0f55c355
ttaehyun Yoo <rth0824@naver.com>
2022-05-22 15:39:05 +0900

    ✅ dictionary study

---
+++ Dictionary/go.mod
@@ -0,0 +1,3 @@
+module github.com/ttaehyun/gonomad/Dictionary
+
+go 1.18
---
+++ Dictionary/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ttaehyun/gonomad/Dictionary/mydict"
+)
+
+func main() {
+	dictionary := mydict.Dictionary{}
+	word := "hello"
+	definition := "Greeting"
+	err := dictionary.Add(word, definition)
+	if err != nil {
+		fmt.Println(err)
+	}
+	hello, _ := dictionary.Search(word)
+	fmt.Println("found", word, "definition", hello)
+	err2 := dictionary.Add(word, definition)
+	if err2 != nil {
+		fmt.Println(err2)
+	}
+}
