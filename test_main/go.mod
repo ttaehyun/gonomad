438414352c084b2ffb0025fe3b458c320fb7ec7f
ttaehyun Yoo <rth0824@naver.com>
2022-07-19 21:30:38 +0900

    🔒️  test용

---
+++ test_main/go.mod
@@ -0,0 +1,3 @@
+module github.com/ttaehyun/gonomad/test_main
+
+go 1.18
---
+++ test_main/main.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/ttaehyun/gonomad/nomadcoin/utils"
+)
+
+const port string = ":4000"
+
+type URLDescription struct {
+	URL         string
+	Method      string
+	Description string
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription{
+		{
+			URL:         "/",
+			Method:      "GET",
+			Description: "See Documentation",
+		},
+	}
+	b, err := json.Marshal(data)
+	utils.HandleErr(err)
+	fmt.Printf("%s", b)
+}
+
+func main() {
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Listening on http://localhost%s", port)
+	log.Fatal(http.ListenAndServe(port, nil))
+}
